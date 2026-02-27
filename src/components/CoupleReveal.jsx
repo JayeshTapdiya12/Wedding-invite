84a3b67e2be7c582c4547a916f532613fe6de1a4
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2026-02-27 23:48:35 +0530

    CountDown in revael partr

--- src/components/CoupleReveal.jsx
+++ src/components/CoupleReveal.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import '../style/CoupleReveal.css';
 import CountDown from './CountDown';
 
-const CoupleReveal = ({ onComplete }) => {
+const CoupleReveal = () => {
     const [animate, setAnimate] = useState(false);
 
     useEffect(() => {
